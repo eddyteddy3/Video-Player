d332b5f817c146c4e2e7920acd484165a1159366
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-03-19 23:51:14 +0500

    Initial Commit

---
+++ Video Player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Video Player
+//
+//  Created by Moazzam Tahir on 19/03/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
