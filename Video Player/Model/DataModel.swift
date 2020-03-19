90dacc8be5ce531ed9f3188111ca8afd48b7a99f
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-03-20 00:05:09 +0500

    added data model for the api

---
+++ Video Player/Model/DataModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataModel.swift
+//  Video Player
+//
+//  Created by Moazzam Tahir on 20/03/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+struct Post: Codable, Identifiable {
+    let id = UUID()
+    var name: String
+    var description: String
+    var thumbnail: String
+    var video_link: String
+}
---
+++ Video Player/Model/Model.swift
@@ -0,0 +1,21 @@
+//
+//  Model.swift
+//  Video Player
+//
+//  Created by Moazzam Tahir on 20/03/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+import SwiftUI
+
+struct Model: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Model_Previews: PreviewProvider {
+    static var previews: some View {
+        Model()
+    }
+}
