18930a24e4980cf7ca20c5ab939059f520219a12
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-03-21 13:58:22 +0500

    imageVIew Class implemented

--- Video Player/Model/ImageModel.swift
+++ Video Player/Model/ImageModel.swift
@@ -9,4 +9,25 @@
 import Foundation
 import SwiftUI
 
-
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    var imageURL: String?
+    
+    init(url: String) {
+        self.imageURL = url
+        loadImage()
+    }
+    
+    func loadImage() {
+        guard let url = URL(string: imageURL!) else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            if let data = data {
+                let image = UIImage.init(data: data)
+                
+                DispatchQueue.main.async {
+                    self.image = image
+                }
+            }
+        }
+    }
+}
---
+++ Video Player/Views/ImageView.swift
@@ -0,0 +1,32 @@
+//
+//  ImageView.swift
+//  Video Player
+//
+//  Created by Moazzam Tahir on 21/03/2020.
+//  Copyright © 2020 Moazzam Tahir. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    @ObservedObject var image: ImageLoader
+    static var placeholderImage = UIImage.init(systemName: "exclamationmark.icloud.fill")
+    
+    init(imageUrl: String) {
+        image = ImageLoader(url: imageUrl)
+    }
+    
+    var body: some View {
+        Image(uiImage: image.image ?? ImageView.placeholderImage!)
+        .resizable()
+        .frame(width: 40, height: 40)
+    }
+    
+    
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView(imageUrl: nil)
+    }
+}
